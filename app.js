c9416c48ad34520407866a6bd6a5f957668c326f
billydf <kyo.bil@gmail.com>
2019-04-21 00:47:39 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const builder = require('botbuilder');
+const restify = require('restify');
+const utils = require('./utils.js');
+const customVisionService = require('./customVisionService.js');
+const azure = require('botbuilder-azure');
+
+// Create chat connector for communicating with the Bot Framework Service
+const connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Setup Restify Server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+
+var documentDbOptions = {
+    host: 'https://azbot.documents.azure.com:443/', 
+    masterKey: 'Lx7yP3TrLTalLEht81G6jXiuer2ILrFuBZhpHiJ1p0XIc3Llo0UG3tVvucpjLgPzRSPmB92QLAiWpRX9EHR3bQ==', 
+    database: 'botdbid',   
+    collection: 'botdata'
+};
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+const bot = new builder.UniversalBot(connector);
+
+// default dialog
+bot.dialog('/', function(session) {
+    if(utils.hasImageAttachment(session)){
+        var stream = utils.getImageStreamFromMessage(session.message); 
+        customVisionService.predict(stream)
+            .then(function (response) {
+                // Convert buffer into string then parse the JSON string to object
+                var jsonObj = JSON.parse(response.toString('utf8'));
+                console.log(jsonObj);
+                var topPrediction = jsonObj["Predictions"][0];
+
+                // make sure we only get confidence level with 0.80 and above. But you can adjust this depending on your need
+                if (topPrediction.Probability >= 0.80) {
+                    session.send(`Hey, I think this image is a ${topPrediction.Tag}!`);
+                } else {
+                    session.send('Sorry! I don\'t know what that is :(');
+                }
+            }).catch(function (error) {
+                console.log(error);
+                session.send('Oops, there\'s something wrong with processing the image. Please try again.');
+            });
+
+    } else {
+        session.send('I did not receive any image');
+    }
+});
---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../depok175.zip');
+var kuduApi = 'https://depok175.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$depok175';
+var password = 'vkRqCu6hhlA7YfB3eAvhxzTiKd9c4kaCfXifKishcLRaeTNvyehC9MEJnrdg';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('depok175 publish');
+  } else {
+    console.error('failed to publish depok175', err);
+  }
+});
